845edbf37bdf4796b967f86159884dd530f0f8d6
Paweł Gościcki <pawel.goscicki@gmail.com>
2024-10-06 11:23:15 +0200

    Add some LSP mapping for neovim [vim]

--- .vim/lua/init.lua
+++ .vim/lua/init.lua
@@ -237,6 +237,11 @@ lspconfig.ruby_lsp.setup({
   on_attach = function(client, bufnr)
     -- Disable semantic tokens (syntax highlighting via LSP)
     client.server_capabilities.semanticTokensProvider = nil
+
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
   end,
   settings = {
     rubocop = {
